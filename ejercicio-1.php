95f65c9de0a7a65dcd1c639a02bc3e9e66e814a2
Jeffrey Alvares <74696095+Jeff-Alvares@users.noreply.github.com>
2022-03-18 13:52:10 -0600

    Create ejercicio-1.php

---
+++ ejercicio-1.php
@@ -0,0 +1,103 @@
+<html>
+	<head></head>
+	<title>Ejercicio1</title>
+<body>
+		<table>
+			<td>
+				<?php
+				//TABLA DE MULTIPLICAR #2
+					$tabla = array(array("x","="),array(1,2,3,4,5,6,7,8,9,10),array(2,4,6,8,10,12,14,16,18,20));
+					echo $tabla[1][1].$tabla[0][0].$tabla[1][0].$tabla[0][1].$tabla[2][0]."<br/>";
+					echo $tabla[1][1].$tabla[0][0].$tabla[1][1].$tabla[0][1].$tabla[2][1]."<br/>";
+					echo $tabla[1][1].$tabla[0][0].$tabla[1][2].$tabla[0][1].$tabla[2][2]."<br/>";
+					echo $tabla[1][1].$tabla[0][0].$tabla[1][3].$tabla[0][1].$tabla[2][3]."<br/>";
+					echo $tabla[1][1].$tabla[0][0].$tabla[1][4].$tabla[0][1].$tabla[2][4]."<br/>";
+					echo $tabla[1][1].$tabla[0][0].$tabla[1][5].$tabla[0][1].$tabla[2][5]."<br/>";
+					echo $tabla[1][1].$tabla[0][0].$tabla[1][6].$tabla[0][1].$tabla[2][6]."<br/>";
+					echo $tabla[1][1].$tabla[0][0].$tabla[1][7].$tabla[0][1].$tabla[2][7]."<br/>";
+					echo $tabla[1][1].$tabla[0][0].$tabla[1][8].$tabla[0][1].$tabla[2][8]."<br/>";
+					echo $tabla[1][1].$tabla[0][0].$tabla[1][9].$tabla[0][1].$tabla[2][9]."<br/>";
+				?>
+			</td>
+			<td>
+				<?php
+				//TABLA DE MULTIPLICAR #3
+					$tabla = array(array("x","="),array(1,2,3,4,5,6,7,8,9,10),array(3,6,9,12,15,18,21,24,27,30));
+					echo $tabla[1][2].$tabla[0][0].$tabla[1][0].$tabla[0][1].$tabla[2][0]."<br/>";
+					echo $tabla[1][2].$tabla[0][0].$tabla[1][1].$tabla[0][1].$tabla[2][1]."<br/>";
+					echo $tabla[1][2].$tabla[0][0].$tabla[1][2].$tabla[0][1].$tabla[2][2]."<br/>";
+					echo $tabla[1][2].$tabla[0][0].$tabla[1][3].$tabla[0][1].$tabla[2][3]."<br/>";
+					echo $tabla[1][2].$tabla[0][0].$tabla[1][4].$tabla[0][1].$tabla[2][4]."<br/>";
+					echo $tabla[1][2].$tabla[0][0].$tabla[1][5].$tabla[0][1].$tabla[2][5]."<br/>";
+					echo $tabla[1][2].$tabla[0][0].$tabla[1][6].$tabla[0][1].$tabla[2][6]."<br/>";
+					echo $tabla[1][2].$tabla[0][0].$tabla[1][7].$tabla[0][1].$tabla[2][7]."<br/>";
+					echo $tabla[1][2].$tabla[0][0].$tabla[1][8].$tabla[0][1].$tabla[2][8]."<br/>";
+					echo $tabla[1][2].$tabla[0][0].$tabla[1][9].$tabla[0][1].$tabla[2][9]."<br/>";
+				?>
+			</td>
+			<td>
+				<?php
+				//TABLA DE MULTIPLICAR #4
+					$tabla = array(array("x","="),array(1,2,3,4,5,6,7,8,9,10),array(4,8,12,16,20,24,28,32,36,40));
+					echo $tabla[1][3].$tabla[0][0].$tabla[1][0].$tabla[0][1].$tabla[2][0]."<br/>";
+					echo $tabla[1][3].$tabla[0][0].$tabla[1][1].$tabla[0][1].$tabla[2][1]."<br/>";
+					echo $tabla[1][3].$tabla[0][0].$tabla[1][2].$tabla[0][1].$tabla[2][2]."<br/>";
+					echo $tabla[1][3].$tabla[0][0].$tabla[1][3].$tabla[0][1].$tabla[2][3]."<br/>";
+					echo $tabla[1][3].$tabla[0][0].$tabla[1][4].$tabla[0][1].$tabla[2][4]."<br/>";
+					echo $tabla[1][3].$tabla[0][0].$tabla[1][5].$tabla[0][1].$tabla[2][5]."<br/>";
+					echo $tabla[1][3].$tabla[0][0].$tabla[1][6].$tabla[0][1].$tabla[2][6]."<br/>";
+					echo $tabla[1][3].$tabla[0][0].$tabla[1][7].$tabla[0][1].$tabla[2][7]."<br/>";
+					echo $tabla[1][3].$tabla[0][0].$tabla[1][8].$tabla[0][1].$tabla[2][8]."<br/>";
+					echo $tabla[1][3].$tabla[0][0].$tabla[1][9].$tabla[0][1].$tabla[2][9]."<br/>";
+				?>
+			</td>
+			<td>
+				<?php
+				//TABLA DE MULTIPLICAR #5
+					$tabla = array(array("x","="),array(1,2,3,4,5,6,7,8,9,10),array(5,10,15,20,25,30,35,40,45,50));
+					echo $tabla[1][4].$tabla[0][0].$tabla[1][0].$tabla[0][1].$tabla[2][0]."<br/>";
+					echo $tabla[1][4].$tabla[0][0].$tabla[1][1].$tabla[0][1].$tabla[2][1]."<br/>";
+					echo $tabla[1][4].$tabla[0][0].$tabla[1][2].$tabla[0][1].$tabla[2][2]."<br/>";
+					echo $tabla[1][4].$tabla[0][0].$tabla[1][3].$tabla[0][1].$tabla[2][3]."<br/>";
+					echo $tabla[1][4].$tabla[0][0].$tabla[1][4].$tabla[0][1].$tabla[2][4]."<br/>";
+					echo $tabla[1][4].$tabla[0][0].$tabla[1][5].$tabla[0][1].$tabla[2][5]."<br/>";
+					echo $tabla[1][4].$tabla[0][0].$tabla[1][6].$tabla[0][1].$tabla[2][6]."<br/>";
+					echo $tabla[1][4].$tabla[0][0].$tabla[1][7].$tabla[0][1].$tabla[2][7]."<br/>";
+					echo $tabla[1][4].$tabla[0][0].$tabla[1][8].$tabla[0][1].$tabla[2][8]."<br/>";
+					echo $tabla[1][4].$tabla[0][0].$tabla[1][9].$tabla[0][1].$tabla[2][9]."<br/>";
+				?>
+			</td>
+			<td>
+			<?php
+				//TABLA DE MULTIPLICAR #6
+					$tabla = array(array("x","="),array(1,2,3,4,5,6,7,8,9,10),array(6,12,18,24,30,36,42,48,54,60));
+					echo $tabla[1][5].$tabla[0][0].$tabla[1][0].$tabla[0][1].$tabla[2][0]."<br/>";
+					echo $tabla[1][5].$tabla[0][0].$tabla[1][1].$tabla[0][1].$tabla[2][1]."<br/>";
+					echo $tabla[1][5].$tabla[0][0].$tabla[1][2].$tabla[0][1].$tabla[2][2]."<br/>";
+					echo $tabla[1][5].$tabla[0][0].$tabla[1][3].$tabla[0][1].$tabla[2][3]."<br/>";
+					echo $tabla[1][5].$tabla[0][0].$tabla[1][4].$tabla[0][1].$tabla[2][4]."<br/>";
+					echo $tabla[1][5].$tabla[0][0].$tabla[1][5].$tabla[0][1].$tabla[2][5]."<br/>";
+					echo $tabla[1][5].$tabla[0][0].$tabla[1][6].$tabla[0][1].$tabla[2][6]."<br/>";
+					echo $tabla[1][5].$tabla[0][0].$tabla[1][7].$tabla[0][1].$tabla[2][7]."<br/>";
+					echo $tabla[1][5].$tabla[0][0].$tabla[1][8].$tabla[0][1].$tabla[2][8]."<br/>";
+					echo $tabla[1][5].$tabla[0][0].$tabla[1][9].$tabla[0][1].$tabla[2][9]."<br/>";
+				?>
+			<td>
+				<?php
+				//TABLA DE MULTIPLICAR #8
+					$tabla = array(array("x","="),array(1,2,3,4,5,6,7,8,9,10),array(8,16,24,32,40,48,56,64,72,80));
+					echo $tabla[1][7].$tabla[0][0].$tabla[1][0].$tabla[0][1].$tabla[2][0]."<br/>";
+					echo $tabla[1][7].$tabla[0][0].$tabla[1][1].$tabla[0][1].$tabla[2][1]."<br/>";
+					echo $tabla[1][7].$tabla[0][0].$tabla[1][2].$tabla[0][1].$tabla[2][2]."<br/>";
+					echo $tabla[1][7].$tabla[0][0].$tabla[1][3].$tabla[0][1].$tabla[2][3]."<br/>";
+					echo $tabla[1][7].$tabla[0][0].$tabla[1][4].$tabla[0][1].$tabla[2][4]."<br/>";
+					echo $tabla[1][7].$tabla[0][0].$tabla[1][5].$tabla[0][1].$tabla[2][5]."<br/>";
+					echo $tabla[1][7].$tabla[0][0].$tabla[1][6].$tabla[0][1].$tabla[2][6]."<br/>";
+					echo $tabla[1][7].$tabla[0][0].$tabla[1][7].$tabla[0][1].$tabla[2][7]."<br/>";
+					echo $tabla[1][7].$tabla[0][0].$tabla[1][8].$tabla[0][1].$tabla[2][8]."<br/>";
+					echo $tabla[1][7].$tabla[0][0].$tabla[1][9].$tabla[0][1].$tabla[2][9]."<br/>";
+				?>
+			</td>
+		</table>
+	</body>
+</html>
